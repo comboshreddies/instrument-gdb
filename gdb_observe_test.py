d7589c96ebe18b679b90c89ec24bb4b7db67b55e
comboshreddies <Combo Shreddies>
2024-12-27 16:02:13 +0100

    pep8 pylint update

--- gdb_observe_test.py
+++ gdb_observe_test.py
@@ -1,17 +1,44 @@
-def exit_handler (event):
-    print ("event type: exit")
-    if hasattr (event, 'exit_code'):
-        print ("exit code: %d" % (event.exit_code))
+""" tool for instrumeting gdb execution via python """
+
+def exit_handler(event):
+    """ exit event handler """
+    print("event type: exit")
+    if hasattr(event, 'exit_code'):
+        print(f"exit code: {event.exit_code}d")
     else:
-        print ("exit code not available")
+        print("exit code not available")
     gdb.execute("quit")
 
-gdb.events.exited.connect (exit_handler)
 
-def stop_handler (event):
-    print ("event type: stop")
+def stop_handler(_):
+    """ stop event handler """
+    print("event type: stop")
 
-gdb.events.stop.connect (stop_handler)
+
+def get_n_bytes_from_reg(n):
+    """ get n bytes from pointer pointed by register """
+    arch = gdb.selected_frame().architecture().name()
+    if arch in reg_from_arch:
+        reg = reg_from_arch[arch]
+        return (
+            gdb.selected_frame().read_register(reg).cast(
+                gdb.lookup_type('char').pointer()).string())[:n]
+    return ""
+
+
+# run this after specifying file <binary>
+def detect_target_arch():
+    """ detect architecture """
+    target_lines = gdb.execute("info target", True, True).split('\n')
+    for i in target_lines:
+        words = i.split(' ')
+        if words[-2] == "type" and words[-3] == "file":
+            return words[-1][:-1]
+    return ""
+
+
+gdb.events.exited.connect(exit_handler)
+gdb.events.stop.connect(stop_handler)
 
 reg_from_arch = {}
 reg_from_arch["i386:x86-64"] = "rsi"
@@ -20,29 +47,12 @@ reg_from_arch["aarch64"] = "x1"
 # catching write syscall on intel/arm
 syscall_from_arch = {}
 syscall_from_arch["elf64-x86-64"] = "1"
-syscall_from_arch["elf64-littleaarch64"] = "64" 
+syscall_from_arch["elf64-littleaarch64"] = "64"
 
-def get_n_bytes_from_reg(n) :
-    arch = gdb.selected_frame().architecture().name()
-    if arch in reg_from_arch :
-        reg = reg_from_arch[arch]
-        return (gdb.selected_frame().read_register(reg).cast(gdb.lookup_type('char').pointer()).string())[:n]
-    return("")
+MYEXEFILE = "./gdb_test"
+print("### preparing to run: " + MYEXEFILE)
 
-### run this after specifying file <binary>
-def detect_target_arch():
-    target_lines = gdb.execute("info target",True,True).split('\n')
-    for i in target_lines :
-        words = i.split(' ')
-        if words[-2] == "type" and words[-3] == "file" :
-            return( words[-1][:-1] )
-    return("")
-
-
-myexefile = "./gdb_test"
-print("### preparing to run: " + myexefile)
-
-gdb.execute("file " + myexefile)
+gdb.execute("file " + MYEXEFILE)
 
 target = detect_target_arch()
 
@@ -56,9 +66,10 @@ gdb.execute("show logging")
 print("### running ")
 gdb.execute("run")
 
-# do nothing until we observe write with 'aaa 70' payload in register on catched syscall
-while get_n_bytes_from_reg(6) != "aaa 70" :
-  gdb.execute("continue")
+# do nothing until we observe write with 'aaa 70'
+# payload in register on catched syscall
+while get_n_bytes_from_reg(6) != "aaa 70":
+    gdb.execute("continue")
 
 frame_to_observe = gdb.selected_frame().name()
 # syscalls do have call and return break, so one more continue
@@ -68,18 +79,18 @@ print("### observing, step by step execution from 70 to 90")
 print("### waiting for frame " + frame_to_observe + " and arg 'aaa 90'")
 
 # we observe step by step, until we spot write syscall with arg 'aaa 90'
-while not ( gdb.selected_frame().name() == frame_to_observe and get_n_bytes_from_reg(6) == "aaa 90" ) :
-  gdb.execute("backtrace 12")
-  gdb.execute("info local")
-  gdb.execute("info args")
-  gdb.execute("info frame")
-  print("### step")
-  gdb.execute("step")
+while not (
+         gdb.selected_frame().name() == frame_to_observe and
+         get_n_bytes_from_reg(6) == "aaa 90"):
+    gdb.execute("backtrace 12")
+    gdb.execute("info local")
+    gdb.execute("info args")
+    gdb.execute("info frame")
+    print("### step")
+    gdb.execute("step")
 
 # we exited section of execution that we wanted to observe in details
 gdb.execute("delete breakpoint 1")
 gdb.execute("continue")
 
 gdb.execute("quit")
-
-
